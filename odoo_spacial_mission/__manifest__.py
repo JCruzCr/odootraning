c17e0d7007f9d9aa20cd83b6e2ba6ec07b836987
JCruzCr <JCruzCr@iingen.unam.mx>
2021-04-13 16:52:01 +0000

    Se agrega modulo de mision espacial

---
+++ odoo_spacial_mission/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Spacial Mission',
+    'version': '14.0.1.0',
+    'summary': """
+        Odoo Inc. Odoo Spacial Mission
+    """,
+    'description': """
+        This module will help to Odoo Inc.
+    """,
+    'author': 'IINGEN',
+    'application': True,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+    
+    
+    
+    
+}
