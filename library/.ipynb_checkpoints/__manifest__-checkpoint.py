b7e9c48ad4a02330556cc4ce9e64a9a8538f53c5
JCruzCr <JCruzCr@iingen.unam.mx>
2021-04-13 21:25:29 +0000

    Se agregan dos módulos; library, volunteer

---
+++ library/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- encodig: utf-8 -*-
+
+{
+    'name': 'Library',
+    'description': """
+    """,
+    'summary': 'Local Library',
+    'version': '14.0.0.1.0',
+    'application': True,
+    
+    'depends': [
+        'base',
+    ],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+    
+}
